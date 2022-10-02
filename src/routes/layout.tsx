0e14693d5a499c44e4c4f916c35706d9bebe844e
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-10-02 12:58:09 +1100

    add socials to footer

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,18 +1,32 @@
 import { component$, Slot } from '@builder.io/qwik'
+import { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
+  const socials = [
+    { label: 'Github', href: 'https://github.com/Danny-Duck' },
+    { label: 'LinkedIn', href: 'https://linkedin.com/in/dannylowater/' },
+    { label: 'Email', href: '' },
+  ]
   return (
     <>
       <main>
-        <section>
-          <Slot />
-        </section>
+        <Slot />
       </main>
       <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-        </a>
+        <h2 class="text-xl">Socials</h2>
+        <p>You can find me here</p>
+        <div>
+          {socials.map(({ label, href }) => (
+            <a target="_blank" class={'mr-2'} href={href}>
+              {label}
+            </a>
+          ))}
+        </div>
       </footer>
     </>
   )
 })
+
+export const head: DocumentHead = {
+  title: 'Danny Thomas',
+}
