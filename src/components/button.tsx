37953376c1d814a9acaf5f0c6ee8beea7a0ed843
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-10-03 17:23:07 +1100

    fix weird button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,7 +5,9 @@ type Props = { onClick$: PropFunction<() => void>; isSelected: boolean }
 export const Button = component$(({ onClick$, isSelected }: Props) => {
   return (
     <button
-      class={`ml-2 pr-2 hover:underline ${isSelected ? 'text-orange-600' : ''}`}
+      class={`ml-2 pr-2 hover:underline text-left ${
+        isSelected ? 'text-orange-600' : ''
+      }`}
       // eslint-disable-next-line
       onClick$={onClick$}
     >
