faf9e5d70957040a42b9aaf22f011c35d7ebc987
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-10-12 18:39:51 +1100

    update header copy

--- src/components/index-header.tsx
+++ src/components/index-header.tsx
@@ -1,19 +1,17 @@
 import { component$ } from '@builder.io/qwik'
 
 export const IndexHeader = component$(() => {
+  const hi = "Hi I'm Danny,"
+  const introCopy = `I develop things for the web, tinker with electronics and I really enjoy solving problems.
+    I'm currently working on a few projects, one of which is this site. `
   return (
     <>
       <div
         class={'hidden lg:flex self-center w-full justify-between items-center'}
       >
         <div class={'pt-10'}>
-          <h1 class="text-2xl">Hi I'm Danny,</h1>
-          <p>
-            I develop things for the web and I really like solving problems.
-          </p>
-          <p>
-            I'm currently in Melbourne, Australia and working at Culture Amp.
-          </p>
+          <h1 class="text-2xl">{hi}</h1>
+          <p class="whitespace-pre-line">{introCopy}</p>
         </div>
         <img
           src="/me.webp"
@@ -24,11 +22,10 @@ export const IndexHeader = component$(() => {
 
       <div class={'flex items-center lg:hidden flex-col self-center md:w-3/4'}>
         <div class="flex items-center justify-between">
-          <h1 class="text-2xl">Hi I'm Danny,</h1>
+          <h1 class="text-2xl">{hi}</h1>
           <img src="/me.webp" class={'w-48 h-48'} />
         </div>
-        <p>I develop things for the web and I really like solving problems.</p>
-        <p>I'm currently in Melbourne, Australia and working at Culture Amp.</p>
+        <p class="whitespace-pre-line">{introCopy}</p>
       </div>
     </>
   )
