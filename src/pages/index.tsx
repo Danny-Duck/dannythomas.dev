9caff8c99320def2024f81cccbd8991d8a954815
Danny Lowater 🫶 <git@dannythomas.dev>
2023-04-07 16:25:35 +1000

    feat: rename site head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>{'dannythomas.dev'}</title>
+        <title>{'Danny Thomas'}</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
